b493253bccda3f8e1989ee4a553b9446927e20cb
AmirAli6578 <amirah648887@gmail.com>
2022-01-15 12:37:50 +0330

    15/1/2022
    change files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("form");
-const username = document.getElementById("username");
+const userName = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
@@ -20,8 +20,7 @@ function showSuccess(input) {
 
 // Check Email is valid
 function checkEmail(input) {
-    const re =
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
         showSuccess(input);
     } else {
@@ -31,7 +30,7 @@ function checkEmail(input) {
 
 // Check required fields
 function checkRequired(inputArr) {
-    inputArr.forEach(function (input) {
+    inputArr.forEach((input) => {
         if (input.value.trim() === "") {
             showError(input, `${getFieldName(input)} is required`);
         } else {
@@ -55,26 +54,20 @@ function getFieldName(input) {
 // Check input length
 function checkLength(input, min, max) {
     if (input.value.length < min) {
-        showError(
-            input,
-            `${getFieldName(input)} must be at least ${min} characters`
-        );
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
     } else if (input.value.length > max) {
-        showError(
-            input,
-            `${getFieldName(input)} must be less than ${max} characters`
-        );
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
     } else {
         showSuccess(input);
     }
 }
 
 // Event Listener
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    checkRequired([username, email, password, password2]);
-    checkLength(username, 3, 15);
+    checkRequired([userName, email, password, password2]);
+    checkLength(userName, 3, 15);
     checkLength(password, 6, 25);
     checkEmail(email);
     checkPasswordMath(password, password2);
